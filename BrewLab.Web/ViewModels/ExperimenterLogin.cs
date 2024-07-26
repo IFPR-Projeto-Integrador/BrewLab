3a11d15c2a48ee339d39985a6eaaeafdbe4ea86a
yuri-almir-pinto <yurialmirp@gmail.com>
2024-07-26 14:59:37 -0300

    Atualizar pasta "Account"
    Atualizado a pasta "Account" com uma tela de registro, e atualizado o razor "Login" para utilizar das DTOs definidos no projeto "Common".

--- BrewLab.Web/ViewModels/ExperimenterLogin.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BrewLab.Web.ViewModels;
-
-public class ExperimenterLogin
-{
-    public string UserName { get; set; } = "";
-    public string Password { get; set; } = "";
-    public string Email { get; set; } = "";
-    public string Name { get; set; } = "";
-}
