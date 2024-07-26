7971b3c87acbc4dea05e5933fd6a644acfa733ac
yuri-almir-pinto <yurialmirp@gmail.com>
2024-07-26 15:24:23 -0300

    Atualizar "Experimenter"
    Atualizado a classe Modelo "Experimenter" para conter a data de criação, atualização, e ser excluível virtualmente.

--- BrewLab.Models/Migrations/20240726182251_ExperimenterLoggedAndVirtualDeletable.Designer.cs
+++ BrewLab.Models/Migrations/20240726182251_ExperimenterLoggedAndVirtualDeletable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BrewLab.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BrewLab.Models.Migrations
 {
     [DbContext(typeof(BrewLabContext))]
-    partial class BrewLabContextModelSnapshot : ModelSnapshot
+    [Migration("20240726182251_ExperimenterLoggedAndVirtualDeletable")]
+    partial class ExperimenterLoggedAndVirtualDeletable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- BrewLab.Models/Models/Experimenter.cs
+++ BrewLab.Models/Models/Experimenter.cs
@@ -3,9 +3,15 @@ using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
 
 namespace BrewLab.Models.Models;
-public class Experimenter : IdentityUser<int>, IBrewLabModel<int>
+public class Experimenter : IdentityUser<int>, IBrewLabModel<int>, ILoggedEntity, IVirtualDeleteable
 {
     [Required]
     [MaxLength(75)]
     public string Name { get; set; } = "";
+    [Required]
+    public DateTime? CreatedAt { get; set; } = DateTime.Now;
+    [Required]
+    public DateTime? UpdatedAt { get; set; } = DateTime.Now;
+    [Required]
+    public bool Deleted { get; set; } = false;
 }
