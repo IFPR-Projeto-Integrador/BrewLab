3725107886d3c13be2673f21217068fb7b8be1ca
yuri-almir-pinto <yurialmirp@gmail.com>
2024-07-31 16:47:47 -0300

    Adicionar Implementações customizadas
    Adicionado implementações customizadas de UserManager e SignInManager, para levar em consideração os usuários virtualmente deletados.

---
+++ Repository/Contracts/IExperimenterSignInManager.cs
@@ -0,0 +1,15 @@
+﻿using BrewLab.Models.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrewLab.Services.Contracts;
+public interface IExperimenterSignInManager
+{
+    public Task<SignInResult> CheckPasswordSignInAsync(Experimenter user, string password, bool lockoutOnFailure);
+    public Task SignInAsync(Experimenter user, bool isPersistent, string? authenticationMethod = null);
+    
+}
---
+++ Repository/Implementations/ExperimenterManager.cs
@@ -0,0 +1,23 @@
+﻿using BrewLab.Models.Models;
+using BrewLab.Services.Contracts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrewLab.Services.Implementations;
+public class ExperimenterManager(IUserStore<Experimenter> store, IOptions<IdentityOptions> optionsAccessor, IPasswordHasher<Experimenter> passwordHasher, IEnumerable<IUserValidator<Experimenter>> userValidators, IEnumerable<IPasswordValidator<Experimenter>> passwordValidators, ILookupNormalizer keyNormalizer, IdentityErrorDescriber errors, IServiceProvider services, ILogger<UserManager<Experimenter>> logger) : UserManager<Experimenter>(store, optionsAccessor, passwordHasher, userValidators, passwordValidators, keyNormalizer, errors, services, logger), IExperimenterManager
+{
+    public override async Task<Experimenter?> FindByNameAsync(string userName)
+    {
+        var experimenter = await base.FindByNameAsync(userName);
+
+        if (experimenter is not null && experimenter.Deleted) return null;
+
+        return experimenter;
+    }
+}
