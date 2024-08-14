c00293fa705d59a74d760b4d5f3fb04fb4714cfd
yuri-almir-pinto <yurialmirp@gmail.com>
2024-08-14 16:58:02 -0300

    Adicioanr ExperimentDTO
    Adicioando o arquivo de DTO de experimentos.

---
+++ BrewLab.Common/DTOs/Experiment/ExperimentDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BrewLab.Common.DTOs;
+public static class ExperimentDTO
+{
+    public class View
+    {
+        public int Id { get; set; }
+        public ExperimentalPlanningDTO.View ExperimentalPlanning { get; set; } = new();
+    }
+}
