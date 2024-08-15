ebfbe712870ce7c3539753e7ff5ea0564c76e91b
yuri-almir-pinto <yurialmirp@gmail.com>
2024-08-14 23:42:46 -0300

    Atualizar ExperimentDTO.View com ParsedModel
    Adicionado o modelo parseado a DTO de experimento (Esquecido anteriormente)

--- BrewLab.Common/DTOs/Experiment/ExperimentDTO.cs
+++ BrewLab.Common/DTOs/Experiment/ExperimentDTO.cs
@@ -4,6 +4,7 @@ public static class ExperimentDTO
     public class View
     {
         public int Id { get; set; }
+        public string ParsedModel { get; set; } = "";
         public ExperimentalPlanningDTO.View ExperimentalPlanning { get; set; } = new();
     }
 }
