c7d97e468f2dfba48f9f74c81b2288ccac459eea
yuri-almir-pinto <yurialmirp@gmail.com>
2024-08-21 21:34:53 -0300

    Atualizar ResultDTO UnknownError & CouldNotSendEmail
    Adicionado campos de erro desconhecido e erro de falha ao enviar o email.

--- BrewLab.Common/DTOs/Results/ResultDTO.cs
+++ BrewLab.Common/DTOs/Results/ResultDTO.cs
@@ -8,12 +8,14 @@ public static class ResultDTO
         public IEnumerable<string> Errors { get; set; } = [];
 
         public static readonly Result Succeeded = new() { Success = true };
+        public static readonly Result UnknownError = new() { Success = false, Errors = ["Erro desconhecido."] };
         public static readonly Result InvalidDTO = new() { Success = false, Errors = ["Modelo inválido."] };
         public static readonly Result InvalidIdentification = new() { Success = false, Errors = ["Identificador incorreto."] };
         public static readonly Result ParsingFail = new() { Success = false, Errors = ["Falha ao tentar realizar o parsing."] };
         public static readonly Result IncorrectPassword = new() { Success = false, Errors = ["Senha incorreta."] };
         public static readonly Result ExperimenterDoesNotExist = new() { Success = false, Errors = ["Experimentador não existe."] };
         public static readonly Result RepeatPasswordDoesNotMatch = new() { Success = false, Errors = ["As senhas não são as mesmas."] };
+        public static readonly Result CouldNotSendEmail = new() { Success = false, Errors = ["Não foi possível enviar o email no momento. Tente novamente mais tarde."] };
     }
 
     public class Auth : Result
