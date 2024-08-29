f9b7acbc848d169f0457ff5f1031f1c30736e387
yuri-almir-pinto <yurialmirp@gmail.com>
2024-08-28 21:10:43 -0300

    Criar paleta de cores personalizada.
    Não ta bonito, mas... Fazer o que...

---
+++ BrewLab.Web/Services/Theme.cs
@@ -0,0 +1,29 @@
+﻿using MudBlazor;
+
+namespace BrewLab.Web.Services;
+
+public static class Theme
+{
+    public static readonly MudTheme AppTheme = new()
+    {
+        PaletteLight = new PaletteLight()
+        {
+            Primary = Colors.Pink,
+
+            Surface = Colors.Thistle,
+            Background = Colors.Beige,
+            AppbarBackground = Colors.Blue,
+            DrawerBackground = Colors.Thistle,
+        },
+    };
+
+    public static class Colors
+    {
+        public const string Black = "#222222";
+        public const string White = "#ffffff";
+        public const string Beige = "#f5e6e8";
+        public const string Blue = "#192a51";
+        public const string Pink = "#967aa1";
+        public const string Thistle = "#d5c6e0";
+    }
+}
